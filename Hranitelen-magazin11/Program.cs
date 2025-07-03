cbb34791dbe0f8b024efe033ef47a0cb26113ff6
eange <eange@LENI>
2025-07-03 08:57:27 +0300

    Add project files.

---
+++ Hranitelen-magazin11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hranitelen_magazin11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
