78439f43ef38553b54d06040d97d7870acdd3ada
IT-Career <Emre.Islam198@gmail.com>
2025-07-03 12:14:07 +0300

    Class

--- Hranitelen-magazin11/Class1.cs
+++ Hranitelen-magazin11/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,7 @@ namespace Hranitelen_magazin11
         public string Category { get; set; }
         public int Quantity { get; set; }
         public int ProductId { get; set; }
-        public decimal Price
-        {
-            get { return Price; }
-            set { Price = value; }
-        }
+        
         public Product(string name, string category, int quantity, int productid, decimal price)
         {
             Name = name;
@@ -25,14 +22,26 @@ namespace Hranitelen_magazin11
             ProductId = productid;
             Price = price;
         }
+        public decimal Price
+        {
+            get
+            {
+                return Price;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price must be a positive number.");
+                }
+                Price = value;
+            }
+        }
         public override string ToString()
         {
             return $"{Name} , {Price}, {ProductId},{Quantity},{Category}";
         }
 
-        public void DisplayMenu()
-        {
-            Console.WriteLine($"ID: {ProductId} | Име: {Name} | Категория: {Category} | Цена: {Price:F2} лв | Количество: {Quantity}");
-        }
+       
     }
 }
